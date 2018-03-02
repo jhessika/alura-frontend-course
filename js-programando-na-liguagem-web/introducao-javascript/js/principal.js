d3f44ae14adad120d61abf1f2c6e1399788920f5
Jessica Oliveira <jessica.reformar@gmail.com>
2018-03-02 12:28:08 -0800

    Modificacão do Javascript

---
+++ js-programando-na-liguagem-web/introducao-javascript/js/principal.js
@@ -0,0 +1,79 @@
+
+var titulo = document.querySelector("#titulo");
+titulo.textContent = "Aparecida Nutrição";
+
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoValido = true;
+var alturaValida = true;
+
+
+
+if( peso < 0 || peso >= 1000){
+	console.log("Pesso invalido!");
+	pesoValido = false;
+	tdImc.textContent = "Peso invalido!";
+}
+
+if(altura < 0 || altura >= 3.00){
+	console.log("altura invalida!");
+	alturaValida = false;
+	tdImc.textContent = "Altura invalida!";
+}
+
+if(pesoValido && alturaValida){
+	var imc = peso / (altura * altura);
+	tdImc.textContent = imc;
+}
+
+
+
+
+
+
+
+
+	 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
